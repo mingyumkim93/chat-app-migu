12a674e781b70eba1d7e478ffa5c2920968617c9
MingyumKim <Mingyum93@gmail.com>
2020-04-28 16:41:26 +0300

    deploy #12

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,3 +1,3 @@
 import socketIOClient from "socket.io-client";
-const socket = socketIOClient("http://127.0.0.1:5000");
+const socket = socketIOClient("https://chat-app-migu.herokuapp.com/");
 export default socket;
