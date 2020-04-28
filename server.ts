2c3a1290441c60f57ae9bda3290d357e8ea93f2b
MingyumKim <Mingyum93@gmail.com>
2020-04-28 17:28:42 +0300

    deploy #16

--- server.ts
+++ server.ts
@@ -13,9 +13,9 @@ interface room { id : number, roomName: string, attendees: Array<string>}
 let rooms: Array<room> = [];
 let loggedInUsers: Array<string> = [];
 
-app.get('*', function(req: Request, res: Response) {
-    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
-  });
+// app.get('*', function(req: Request, res: Response) {
+//     res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+//   });
 
 app.get('/', (req: Request, res: Response): void => {
     res.send("hello!");
